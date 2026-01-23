1b59057cee788af91f1e8f81bc48f9976b88a8f6
Erick Seidmann <erickseidmann@gmail.com>
2026-01-23 00:37:42 -0300

    correções finais

--- scripts/place.js
+++ scripts/place.js
@@ -5,16 +5,14 @@ const windChillEl = document.querySelector("#windchill");
 const tempEl = document.querySelector("#temp");
 const windEl = document.querySelector("#wind");
 
-// Static values (must match what is shown in HTML)
 const temperature = Number(tempEl.textContent); // °C
 const windSpeed = Number(windEl.textContent);   // km/h
 
-// Required: one line of code returning the result
 function calculateWindChill(tempC, windKmh) {
     return 13.12 + (0.6215 * tempC) - (11.37 * (windKmh ** 0.16)) + (0.3965 * tempC * (windKmh ** 0.16));
 }
 
-// Required: only calculate if conditions are met (Metric)
+
 function canCalculateWindChill(tempC, windKmh) {
     return tempC <= 10 && windKmh > 4.8;
 }
